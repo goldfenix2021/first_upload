19d730384354e9ebd80e28e37b904cae969d827f
goldfenix2021 <81908710+goldfenix2021@users.noreply.github.com>
2021-04-06 17:03:50 +0200

    Update start.py

--- start.py
+++ start.py
@@ -5,8 +5,10 @@ def run():
   #resu = os.system('bash ./update.sh')
   resus = list()
   res = os.system('apt-get install python3-pip')
+  print('1 komenda:',res)
   resus.append(res)
   res = os.system('python3 -m pip install beautifulsoup4')
+  print('2 komenda:',res)
   resus.append(res)
   return resus
 
